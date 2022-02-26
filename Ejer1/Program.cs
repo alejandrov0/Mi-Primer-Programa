5609c2dce9891109f8b65740c96f1d7719c3f627
clolomagico123 <jvillafuertes@miumg.edu.gt>
2022-02-25 22:55:57 -0600

    mi primer programa

---
+++ Ejer1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ejer1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola mundo este es mi primer programa");
+        }
+    }
+}
